c8882fa3a205f9b3c6218a832313b2f995279642
Avinash Kumar <iavinas@yandex.com>
2024-11-24 12:41:01 +0530

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Avinash Kumar on 24/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
